ccff2205fd725754a4e95cf7bfa455e3e107a57a
lilisha-unity <lisha.li@unity3d.com>
2023-01-05 22:25:51 +0000

    Updated to make code consistent for the simple UI Toolkit example

--- simple-ui-toolkit-workflow/MyCustomEditor.cs
+++ simple-ui-toolkit-workflow/MyCustomEditor.cs
@@ -23,26 +23,26 @@ public class MyCustomEditor : EditorWindow
         // Each editor window contains a root VisualElement object
         VisualElement root = rootVisualElement;
 
-        // VisualElements objects can contain other VisualElement following a tree hierarchy.
+        // VisualElements objects can contain other VisualElements following a tree hierarchy.
         Label label = new Label("These controls were created using C# code.");
         root.Add(label);
 
         Button button = new Button();
         button.name = "button3";
         button.text = "This is button3.";
-        rootVisualElement.Add(button);
+        root.Add(button);
 
         Toggle toggle = new Toggle();
         toggle.name = "toggle3";
         toggle.label = "Number?";
-        rootVisualElement.Add(toggle);
+        root.Add(toggle);
 
         // Import UXML
         var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/MyCustomEditor.uxml");
         VisualElement labelFromUXML = visualTree.Instantiate();
         root.Add(labelFromUXML);
 
-        rootVisualElement.Add(m_UXMLTree.Instantiate());
+        root.Add(m_UXMLTree.Instantiate());
 
         //Call the event handler
         SetupButtonHandler();
@@ -51,7 +51,9 @@ public class MyCustomEditor : EditorWindow
     //Functions as the event handlers for your button click and number counts 
     private void SetupButtonHandler()
     {
-        var buttons = rootVisualElement.Query<Button>();
+        VisualElement root = rootVisualElement;
+
+        var buttons = root.Query<Button>();
         buttons.ForEach(RegisterHandler);
     }
 
@@ -62,6 +64,8 @@ public class MyCustomEditor : EditorWindow
 
     private void PrintClickMessage(ClickEvent evt)
     {
+        VisualElement root = rootVisualElement;
+
         ++m_ClickCount;
 
         //Because of the names we gave the buttons and toggles, we can use the
@@ -69,7 +73,7 @@ public class MyCustomEditor : EditorWindow
         Button button = evt.currentTarget as Button;
         string buttonNumber = button.name.Substring(m_ButtonPrefix.Length);
         string toggleName = "toggle" + buttonNumber;
-        Toggle toggle = rootVisualElement.Q<Toggle>(toggleName);
+        Toggle toggle = root.Q<Toggle>(toggleName);
 
         Debug.Log("Button was clicked!" +
             (toggle.value ? " Count: " + m_ClickCount : ""));
