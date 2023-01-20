6a1450e68fb85e492337114c2d5e67d40654af24
Lisha Li <lisha.li@unity3d.com>
2023-01-20 09:28:21 -0800

    Updated according to documentation change

---
+++ relative-and-absolute-position/PostioningTestRuntime.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PostioningTestRuntime : MonoBehaviour
+{
+    void OnEnable()
+    {
+        GetComponent<UIDocument>();
+
+    }   
+}
+
+        
+
