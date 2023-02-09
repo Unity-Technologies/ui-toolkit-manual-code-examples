2c4b48c4bdd16c6de7a338fa77d6b8acee831490
Lisha Li <lisha.li@unity3d.com>
2023-02-09 14:32:32 -0500

    Fixed C# code error according to a user feedback.

--- radial-progress/RadialProgressComponent.cs
+++ radial-progress/RadialProgressComponent.cs
@@ -10,16 +10,22 @@ public class RadialProgressComponent : MonoBehaviour
 
     RadialProgress m_RadialProgress;
 
-    void start()
+    void Start()
     {
         var root = GetComponent<UIDocument>().rootVisualElement;
-        // Find the custom control by name.
-        m_RadialProgress = root.Q("radial-progress") as RadialProgress;
+
+       m_RadialProgress = new RadialProgress() {
+            style = {
+                position = Position.Absolute,
+                left = 20, top = 20, width = 200, height = 200
+            }
+        };
+
+        root.Add(m_RadialProgress);
     }
 
     void Update()
     {
-        // For demo purpose, give the progress property dynamic values.
         m_RadialProgress.progress = ((Mathf.Sin(Time.time) + 1.0f) / 2.0f) * 60.0f + 10.0f;
     }
 }
