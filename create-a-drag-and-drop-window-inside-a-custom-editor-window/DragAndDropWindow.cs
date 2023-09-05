215b134e8c8f736e7de90a13bceb891a154ac790
Lisha Li <lisha.li@unity3d.com>
2023-09-05 12:33:13 -0700

    Updated code examples for drag and drop UI

--- create-a-drag-and-drop-window-inside-a-custom-editor-window/DragAndDropWindow.cs
+++ create-a-drag-and-drop-window-inside-a-custom-editor-window/DragAndDropWindow.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEditor.UIElements;
 
-
 public class DragAndDropWindow : EditorWindow
 {
     [MenuItem("Window/UI Toolkit/Drag And Drop")]
@@ -19,13 +18,16 @@ public class DragAndDropWindow : EditorWindow
         VisualElement root = rootVisualElement;
 
           // Import UXML
-        var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Drag and Drop/DragAndDropWindow.uxml");
+        var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/DragAndDropWindow.uxml");
         VisualElement labelFromUXML = visualTree.Instantiate();
         root.Add(labelFromUXML);
 
         // A stylesheet can be added to a VisualElement.
         // The style will be applied to the VisualElement and all of its children.
-        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Drag and Drop/DragAndDropWindow.uss");
+        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Editor/DragAndDropWindow.uss");
+
+        DragAndDropManipulator manipulator =
+        new(rootVisualElement.Q<VisualElement>("object"));
     
     }
 }
