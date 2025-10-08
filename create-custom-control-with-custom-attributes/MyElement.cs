3fd8f38a06cf3662cfa263b18a7aace81412374b
lisha-li <lisha.li@unity3d.com>
2025-10-08 10:28:52 -0700

    Updated code sample with default values for custom attributes.

--- create-custom-control-with-custom-attributes/MyElement.cs
+++ create-custom-control-with-custom-attributes/MyElement.cs
@@ -1,11 +1,33 @@
+using UnityEngine;
 using UnityEngine.UIElements;
 
 [UxmlElement]
 partial class MyElement : VisualElement
 {
-    [UxmlAttribute]
-    public string myString { get; set; } = "default_value";
+    string _myString = "default_value";
+    int _myInt = 2;
 
     [UxmlAttribute]
-    public int myInt { get; set; } = 2;
+    public string myString
+    {
+        get => _myString;
+        set
+        {
+            _myString = value;
+            Debug.Log($"myString set to: {_myString}");
+            // React here, e.g. update UI
+        }
+    }
+
+    [UxmlAttribute]
+    public int myInt
+    {
+        get => _myInt;
+        set
+        {
+            _myInt = value;
+            Debug.Log($"myInt set to: {_myInt}");
+            // React here, e.g. update UI
+        }
+    }
 }
