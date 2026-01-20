14472c1995f87353396f646fca6ea1739ecd3fbf
lilisha-unity <94024889+lilisha-unity@users.noreply.github.com>
2026-01-20 21:33:38 +0000

    Moved Get started runtime binding example code to master

---
+++ get-started-runtime-binding/ExampleObject.cs
@@ -0,0 +1,15 @@
+using Unity.Properties;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[CreateAssetMenu]
+public class ExampleObject : ScriptableObject
+{
+    [Header("Simple binding")]
+    [CreateProperty]
+    public string simpleLabel = "Hello World!";
+}
